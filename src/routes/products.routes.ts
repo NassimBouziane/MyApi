3c481bc771d2b4ca5ba8f34cf883e65df5f83434
Jordan <hoarau_j@etna-alternance.net>
2022-10-20 11:38:12 +0200

    routing express v1

---
+++ src/routes/products.routes.ts
@@ -0,0 +1,26 @@
+ express = require('express')
+const routerProducts = express()
+
+
+routerProducts.get('/', (req, res) => {
+    res.send("je suis product")
+  })
+
+routerProducts.get('/:id', (req, res) => {
+    res.send("je suis id")
+  })
+  
+routerProducts.post('/', (req, res) => {
+    res.send("je suis le post")
+  })
+
+routerProducts.patch('/:id', (req, res) => {
+    res.send("je suis le patch")
+  })
+
+routerProducts.delete('/:id', (req, res) => {
+    res.send("je suis delete")
+  })
+   
+  
+module.exports = routerProducts;
