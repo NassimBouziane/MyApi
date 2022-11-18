1886e1d7210dfcedd1152acf404e0bde82a72c94
nassimbouziane <bouzianenassim17102002@gmail.com>
2022-11-18 22:11:35 +0100

    cors fix

--- src/server.ts
+++ src/server.ts
@@ -13,23 +13,18 @@ const port = 3000;
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'http://localhost:3001/',
+  origin: '*',
   optionSuccessStatus: 200,
-  preflightContinue: false,
+  // preflightContinue: false,
 };
 
+
 app.use((req:Request, res:Response, next:Function) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(
   express.urlencoded({
